e96d1b122a6891fc68e1e047a7d52ff78d4ad251
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-05-01 15:59:32 +0200

    small changes

--- module-01/ex00/main.cpp
+++ module-01/ex00/main.cpp
@@ -2,16 +2,15 @@
 #include <cstdlib>
 
 int main (void) {
-
-    Zombie zombie_object("Object");
+    Zombie handler("Handler");
 
     // Create a new zombie on the heap
-    Zombie *new_zombie_ptr = zombie_object.newZombie("Heap");
+    Zombie *new_zombie_ptr = handler.newZombie("Heap");
     new_zombie_ptr->announce();
     delete(new_zombie_ptr);
 
     // Create a new zombie on the stack
-    zombie_object.randomChump("Stack");
+    handler.randomChump("Stack");
 
     return EXIT_SUCCESS;
 }
