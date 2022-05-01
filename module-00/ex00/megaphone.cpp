1b6bb9ab5d9d04e5b4e9c59159634273c25da21b
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-05-01 15:48:30 +0200

    refactor megaphone

--- module-00/ex00/megaphone.cpp
+++ module-00/ex00/megaphone.cpp
@@ -3,18 +3,17 @@
 #include <cstring>
 
 int main(int argc, char **argv) {
-
-    if (argc == 1)
+	if (argc == 1)
+	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-    else
-    {
-    	for (std::size_t i = 1; argv[i]; i++) {
-		    for (std::size_t j = 0; j < std::strlen(argv[i]); j++) {
-                std::cout << (char)std::toupper(argv[i][j]);
-            }
-    	}
-    	std::cout << std::endl;
-    }
+		return EXIT_SUCCESS;
+	}
 
-    return EXIT_SUCCESS;
+	for (std::size_t i = 1; argv[i]; i++) {
+		for (std::size_t j = 0; j < std::strlen(argv[i]); j++) {
+			std::cout << (char)std::toupper(argv[i][j]);
+		}
+	}
+	std::cout << std::endl;
+	return EXIT_SUCCESS;
 }
