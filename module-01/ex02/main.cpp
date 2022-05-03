38dd661ca3c1feba190a9ffa2a7b59367c32cb81
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-05-03 16:06:25 +0200

    ex02

---
+++ module-01/ex02/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+int main(void)
+{
+    std::string brain = "HI THIS IS BRAIN";
+    std::string *stringPTR = &brain;
+    std::string &stringREF = brain;
+    
+    std::cout << "The memory address of the string brain variable is: " << &brain << std::endl;
+    std::cout << "The memory address of the stringPTR is:             " << stringPTR << std::endl;
+    std::cout << "The memory address of the stringREF is:             " << &stringREF << std::endl;
+
+    std::cout << "The value of the string brain variable is:          " << brain << std::endl;
+    std::cout << "The value pointed to by stringPTR is:               " << *stringPTR << std::endl;
+    std::cout << "The value pointed to by stringREF is:               " << stringREF << std::endl;
+
+    return EXIT_SUCCESS;
+}
