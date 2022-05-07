4e145bf6cb91796a7395ac842bc92583b21acdfd
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-05-07 17:46:44 +0200

    google c++ format

--- module-00/ex01/src/Main.cpp
+++ module-00/ex01/src/Main.cpp
@@ -1,19 +1,23 @@
 #include "PhoneBook.hpp"
 
 int main(void) {
+  PhoneBook   phonebook;
+  std::string user_input_string;
 
-	PhoneBook	phonebook;
-	std::string user_input_string;
+  while (true) {
+    std::cout << "Type a command (ADD, SEARCH, EXIT): ";
+    std::getline(std::cin, user_input_string);
 
-	while (true)
-	{
-		std::cout << "Type a command (ADD, SEARCH, EXIT): ";
-		std::getline(std::cin, user_input_string);
-		
-		if (user_input_string == "ADD") { phonebook.addContact(std::cin); }
-		if (user_input_string == "SEARCH") { phonebook.searchContact(std::cin); }
-		if (user_input_string == "EXIT") { break; }
-	}
+    if (user_input_string == "ADD") {
+      phonebook.addContact(std::cin);
+    }
+    if (user_input_string == "SEARCH") {
+      phonebook.searchContact(std::cin);
+    }
+    if (user_input_string == "EXIT") {
+      break;
+    }
+  }
 
-	return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
