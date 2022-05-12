6694ef2f6a4921e95e93f62134d524a5e695d17b
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-05-12 14:34:21 +0200

    removed comment

--- module-01/ex04/src/File.cpp
+++ module-01/ex04/src/File.cpp
@@ -1,9 +1,5 @@
 #include "File.hpp"
 
-// You should instead create the ifstream only when you need it, inside the method that needs it,
-// and let it be destroyed when the method exits.
-// It should not be a part of the object because it has no need to persist with the object.
-
 File::File(){};
 File::File(const char* path) : path(path){};
 File::~File(){};
