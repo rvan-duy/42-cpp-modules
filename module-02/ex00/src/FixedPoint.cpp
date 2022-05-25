b0f57f1e52d7cb8cf2c1eb032f71367c232fe725
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-05-25 14:02:13 +0200

    format

--- module-02/ex00/src/FixedPoint.cpp
+++ module-02/ex00/src/FixedPoint.cpp
@@ -4,17 +4,22 @@ Fixed::Fixed() {
   std::cout << "Default constructor called" << std::endl;
   this->value = 0;
 };
+
 Fixed::Fixed(const Fixed& point) {
   std::cout << "Copy constructor called" << std::endl;
   *this = point;
 };
+
 void Fixed::operator=(const Fixed& point) {
   std::cout << "Copy assignment operator called" << std::endl;
   this->value = point.getRawBits();
-}
-Fixed::~Fixed() { std::cout << "Destructor called" << std::endl; }
+};
+
+Fixed::~Fixed() { std::cout << "Destructor called" << std::endl; };
 
 int Fixed::getRawBits() const {
   std::cout << "getRawBits member function called" << std::endl;
   return this->value;
 };
+
+void Fixed::setRawBits(int const raw) { this->value = raw; };
