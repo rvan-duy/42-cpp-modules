b4a84502c423d98303624e5e1408ecc0cbafeff4
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-06-22 15:29:33 +0200

    init ex02

---
+++ module-04/ex02/src/Animal.cpp
@@ -0,0 +1,25 @@
+#include "Animal.hpp"
+
+Animal::Animal() : type("") {
+  std::cout << "-> (Animal) Default constructor for nameless Animal is called" << std::endl;
+  this->brain = new Brain();
+};
+
+Animal::~Animal() {
+  delete brain;
+  std::cout << "-> (Animal) Destructor for " << this->type << " is called" << std::endl;
+};
+
+Animal::Animal(const Animal &animal) : type(animal.type) {
+  std::cout << "-> (Animal) Copy assignment constructor for " << this->type << " is called" << std::endl;
+};
+
+Animal &Animal::operator=(const Animal &animal) {
+  this->type = animal.type;
+  std::cout << "-> (Animal) Copy assignment constructor for " << this->type << " is called" << std::endl;
+  return *this;
+};
+
+std::string Animal::getType() const { return this->type; };
+
+void        Animal::makeSound() const { std::cout << "-> (Animal) * Animal noises *" << std::endl; }
