c831af7878bfdc85c36f8a3ca73bea869f6d8a20
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-08-02 16:35:01 +0200

    init module 05 ex00

---
+++ module-05/ex00/src/main.cpp
@@ -0,0 +1,41 @@
+#include <cstdlib>
+
+#include "Bureaucrat.hpp"
+
+int main(void) {
+  Bureaucrat human("human", 1);
+
+  std::cout << "TEST 1" << std::endl;
+
+  try {
+    std::cout << human << std::endl;
+    human.incrementGrade(10);
+    std::cout << human << std::endl;
+  } catch (std::exception& e) {
+    std::cout << e.what() << std::endl;
+  };
+
+  std::cout << "TEST 2" << std::endl;
+
+  try {
+    std::cout << human << std::endl;
+    human.decrementGrade(200);
+    std::cout << human << std::endl;
+  } catch (std::exception& e) {
+    std::cout << e.what() << std::endl;
+  };
+
+  std::cout << "TEST 3" << std::endl;
+
+  try {
+    std::cout << human << std::endl;
+    human.decrementGrade(20);
+    std::cout << human << std::endl;
+    human.incrementGrade(10);
+    std::cout << human << std::endl;
+  } catch (std::exception& e) {
+    std::cout << e.what() << std::endl;
+  };
+
+  return EXIT_SUCCESS;
+};
