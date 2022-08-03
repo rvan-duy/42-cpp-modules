c15320df0bf88c1e46871b93b3108df0558a6029
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-08-03 13:01:27 +0200

    format

--- module-01/ex02/main.cpp
+++ module-01/ex02/main.cpp
@@ -1,24 +1,23 @@
-#include <iostream>
 #include <cstdlib>
+#include <iostream>
 #include <string>
 
 // Why use references over pointers?
 // - it can't be reassigned to another object
 // - it can't be NULL
 
-int main(void)
-{
-    std::string brain = "HI THIS IS BRAIN";
-    std::string *stringPTR = &brain;
-    std::string &stringREF = brain;
-    
-    std::cout << "The memory address of the string brain variable is: " << &brain << std::endl;
-    std::cout << "The memory address of the stringPTR is:             " << stringPTR << std::endl;
-    std::cout << "The memory address of the stringREF is:             " << &stringREF << std::endl;
+int main(void) {
+  std::string  brain     = "HI THIS IS BRAIN";
+  std::string *stringPTR = &brain;
+  std::string &stringREF = brain;
 
-    std::cout << "The value of the string brain variable is:          " << brain << std::endl;
-    std::cout << "The value pointed to by stringPTR is:               " << *stringPTR << std::endl;
-    std::cout << "The value pointed to by stringREF is:               " << stringREF << std::endl;
+  std::cout << "The memory address of the string brain variable is: " << &brain << std::endl;
+  std::cout << "The memory address of the stringPTR is:             " << stringPTR << std::endl;
+  std::cout << "The memory address of the stringREF is:             " << &stringREF << std::endl;
 
-    return EXIT_SUCCESS;
+  std::cout << "The value of the string brain variable is:          " << brain << std::endl;
+  std::cout << "The value pointed to by stringPTR is:               " << *stringPTR << std::endl;
+  std::cout << "The value pointed to by stringREF is:               " << stringREF << std::endl;
+
+  return EXIT_SUCCESS;
 }
