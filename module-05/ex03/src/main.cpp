60ca9881ea1999381d78b4edcbc9e368a61cad0e
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-08-11 15:39:19 +0200

    added main

--- module-05/ex03/src/main.cpp
+++ module-05/ex03/src/main.cpp
@@ -1,99 +1,16 @@
 #include <cstdlib>
 
-#include "ShrubberyCreationForm.hpp"
-#include "RobotomyRequestForm.hpp"
+#include "Intern.hpp"
 #include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
 
 int main(void) {
-  std::cout << "TEST 1 - bureaucrat has a high enough grade to sign and execute ShrubberyCreationForm" << std::endl;
   {
-    Bureaucrat bureaucrat("Gerrit", 1);
-    ShrubberyCreationForm form("form");
-
-    bureaucrat.signForm(form);
-    bureaucrat.executeForm(form);
+    Intern someRandomIntern;
+    Form*  rrf;
+    rrf = someRandomIntern.makeForm("robotomy request", "Bender");
   }
-  std::cout << std::endl;
-
-  std::cout << "TEST 2 - bureaucrat has a high enough grade to sign but not execute ShrubberyCreationForm" << std::endl;
-  {
-    Bureaucrat bureaucrat("Gerrit", 140);
-    ShrubberyCreationForm form("form");
-
-    bureaucrat.signForm(form);
-    bureaucrat.executeForm(form);
-  }
-  std::cout << std::endl;
-
-  std::cout << "TEST 3 - bureaucrat does not have an high enough grade to execute nor sign ShrubberyCreationForm" << std::endl;
-  {
-    Bureaucrat bureaucrat("Gerrit", 149);
-    ShrubberyCreationForm form("form");
-
-    bureaucrat.signForm(form);
-    bureaucrat.executeForm(form);
-  }
-  std::cout << std::endl;
-
-  std::cout << "TEST 4 - bureaucrat has a high enough grade to sign and execute RobotomyRequestForm" << std::endl;
-  {
-    Bureaucrat bureaucrat("Gerrit", 1);
-    RobotomyRequestForm form("form");
-
-    bureaucrat.signForm(form);
-    bureaucrat.executeForm(form);
-  }
-  std::cout << std::endl;
-
-  std::cout << "TEST 5 - bureaucrat has a high enough grade to sign but not execute RobotomyRequestForm" << std::endl;
-  {
-    Bureaucrat bureaucrat("Gerrit", 70);
-    RobotomyRequestForm form("form");
-
-    bureaucrat.signForm(form);
-    bureaucrat.executeForm(form);
-  }
-  std::cout << std::endl;
-
-  std::cout << "TEST 6 - bureaucrat does not have an high enough grade to execute nor sign RobotomyRequestForm" << std::endl;
-  {
-    Bureaucrat bureaucrat("Gerrit", 149);
-    RobotomyRequestForm form("form");
-
-    bureaucrat.signForm(form);
-    bureaucrat.executeForm(form);
-  }
-  std::cout << std::endl;
-
-  std::cout << "TEST 7 - bureaucrat has a high enough grade to sign and execute PresidentialPardonForm" << std::endl;
-  {
-    Bureaucrat bureaucrat("Gerrit", 1);
-    PresidentialPardonForm form("form");
-
-    bureaucrat.signForm(form);
-    bureaucrat.executeForm(form);
-  }
-  std::cout << std::endl;
-
-  std::cout << "TEST 8 - bureaucrat has a high enough grade to sign but not execute PresidentialPardonForm" << std::endl;
-  {
-    Bureaucrat bureaucrat("Gerrit", 10);
-    PresidentialPardonForm form("form");
-
-    bureaucrat.signForm(form);
-    bureaucrat.executeForm(form);
-  }
-  std::cout << std::endl;
-
-  std::cout << "TEST 9 - bureaucrat does not have an high enough grade to execute nor sign PresidentialPardonForm" << std::endl;
-  {
-    Bureaucrat bureaucrat("Gerrit", 149);
-    PresidentialPardonForm form("form");
-
-    bureaucrat.signForm(form);
-    bureaucrat.executeForm(form);
-  }
-  std::cout << std::endl;
 
   return EXIT_SUCCESS;
 };
