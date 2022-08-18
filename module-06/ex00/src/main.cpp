265ca48fd1ee60b60106f04a2a2c3c5504312479
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-08-18 14:04:06 +0200

    added isFloat

--- module-06/ex00/src/main.cpp
+++ module-06/ex00/src/main.cpp
@@ -11,10 +11,38 @@
 // - convert it explicitly to the 3 other types
 // - display the result as shown
 
+static bool isFloat(const std::string &str) {
+  bool found_dot = false;
+
+  // Iterate over constant string
+  for (std::string::const_iterator it = str.cbegin(); it != str.cend(); it++) {
+    // Skip first - or +
+    if ((*it == '-' || *it == '+') && it == str.cbegin()) continue;
+
+    // Check for dot
+    if (*it == '.') {
+      if (found_dot == true) return false;
+      found_dot = true;
+      continue;
+    };
+
+    // Check if str is a valid float
+    if (*it == 'f' && it == str.cend() - 1 && found_dot == true) return true;
+
+    if (std::isdigit(*it) == false) return false;
+  };
+
+  return false;
+};
+
+static void printFloat(float num) {
+
+};
+
 static bool isInt(const std::string &str) {
   // Check if string contains anything else but numbers
   for (std::size_t index = 0; index < str.size(); index++) {
-    if (!std::isdigit(str[index])) return false;
+    if (std::isdigit(str[index]) == false) return false;
   };
 
   // Check if integer value is out of range
@@ -22,7 +50,7 @@ static bool isInt(const std::string &str) {
     std::stoi(str);
   } catch (const std::out_of_range &oor) {
     return false;
-  }
+  };
 
   return true;
 };
@@ -54,9 +82,8 @@ static void printChar(char ch) {
 };
 
 int main(int argc, char **argv) {
-  std::string input_string(argv[1]);
-
   if (argc == 2) {
+    std::string input_string(argv[1]);
     if (isChar(input_string) == true) {
       char casted_char = static_cast<char>(input_string[0]);
       printChar(casted_char);
@@ -69,9 +96,13 @@ int main(int argc, char **argv) {
       return EXIT_SUCCESS;
     };
 
-    // if (isFloat(input_string) == true) {
-    //   int casted_float = 
-    // }
+    if (isFloat(input_string) == true) {
+      float casted_float = std::stof(input_string);
+      // int casted_float =
+      return EXIT_SUCCESS;
+    };
+
+    std::cout << "error" << std::endl;
   };
   return EXIT_SUCCESS;
 };
