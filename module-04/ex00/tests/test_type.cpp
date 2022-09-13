12e6a99c8ba21a31a8233312aa0cb85d66f49176
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-09-13 13:32:00 +0200

    fixed tests

--- module-04/ex00/tests/test_type.cpp
+++ module-04/ex00/tests/test_type.cpp
@@ -7,32 +7,32 @@ SCENARIO("Initializing Animal class and testing methods", "[Animal Class]") {
   GIVEN("Animal Class") {
     WHEN("Animal Class is an Animal") {
       Animal* animal = new Animal();
-      THEN("Animal type is empty") { REQUIRE(animal->getType() == ""); };
+      THEN("Animal type is empty") { REQUIRE(animal->getType() == ""); }
       delete animal;
-    };
+    }
     WHEN("Animal Class is a Dog") {
       Animal* dog = new Dog();
-      THEN("Animal type is Dog") { REQUIRE(dog->getType() == "Dog"); };
+      THEN("Animal type is Dog") { REQUIRE(dog->getType() == "Dog"); }
       delete dog;
-    };
+    }
     WHEN("Animal Class is a Cat") {
       Animal* cat = new Cat();
-      THEN("Animal type is Cat") { REQUIRE(cat->getType() == "Cat"); };
+      THEN("Animal type is Cat") { REQUIRE(cat->getType() == "Cat"); }
       delete cat;
-    };
-  };
+    }
+  }
   GIVEN("Dog Class (Derived from Animal)") {
     WHEN("Dog Class is an Animal") {
       Dog* dog = new Dog();
-      THEN("Dog Type is a Dog") { REQUIRE(dog->getType() == "Dog"); };
+      THEN("Dog Type is a Dog") { REQUIRE(dog->getType() == "Dog"); }
       delete dog;
-    };
-  };
+    }
+  }
   GIVEN("Cat Class (Derived from Animal)") {
     WHEN("Cat Class is an Animal") {
       Cat* cat = new Cat();
-      THEN("Cat Type is a Dog") { REQUIRE(cat->getType() == "Cat"); };
+      THEN("Cat Type is a Dog") { REQUIRE(cat->getType() == "Cat"); }
       delete cat;
-    };
-  };
-};
+    }
+  }
+}
